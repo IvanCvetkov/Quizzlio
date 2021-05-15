3fe07302f6d6535de455b1dc1916de5959f63ab3
Ivan Cvetkov <42945545+IvanCvetkov@users.noreply.github.com>
2021-05-15 11:23:30 +0300

    Add files via upload

---
+++ Programming_Quizes/Programming_Quizes/views/AchievementsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Programming_Quizes.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AchievementsPage : ContentPage
+    {
+        public AchievementsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstQuiz_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FiveQuizesFinished_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
